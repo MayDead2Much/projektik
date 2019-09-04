b0c7bd7b5e63aa6dd02fb6687bf88fec05d524b1
Kutilek <flamekick97@gmail.com>
2019-09-04 10:49:32 +0200

    yprava

---
+++ Assets/CamerControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CamerControl : MonoBehaviour 
+{
+	public Transform target;
+	public float smoothSPeed = 0.125f;
+
+	private Vector3 offset;
+
+
+	void Start () 
+	{
+		offset = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = Vector3.Lerp(transform.position, target.position + offset, 0.08f);
+	}
+}
