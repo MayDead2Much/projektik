6407cb574d5b5314354178dd4741b1ea4f11ca4a
maydead2much <maydead2much@gmail.com>
2019-09-04 11:08:41 +0200

    jugoslavie

--- Assets/Stats.cs
+++ Assets/Stats.cs
@@ -2,8 +2,8 @@ using System;
 
 [Serializable]
 public struct Stats {
-    int hp;
-    int mana;
+    public int hp;
+    public int mana;
 
     public Stats(int hp, int mana) {
         this.hp = hp;
