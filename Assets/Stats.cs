c04582594e5b203b2830cae92c6119e5ba2df137
Kutilek <flamekick97@gmail.com>
2019-09-05 12:24:01 +0200

    simple message

--- Assets/Stats.cs
+++ Assets/Stats.cs
@@ -1,13 +1,15 @@
 using System;
 
 [Serializable]
-public struct Stats {
+public struct Stats 
+{
     public int attack;
     public int hp;
     public int mana;
     public double attackSpeed;
 
-    public Stats(int hp, int mana, int attack, double attackSpeed) {
+    public Stats(int hp, int mana, int attack, double attackSpeed) 
+    {
         this.hp = hp;
         this.mana = mana;
         this.attack = attack;
